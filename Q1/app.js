28846a2d437d9fedcfe4778a64df23645b5dff9c
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-04-14 20:31:34 +0300

    Reviewed Assignmrnt -All good

--- Q1/app.js
+++ Q1/app.js
@@ -1,17 +1,15 @@
 class Animal {
+	constructor(name) {
+		this.name = name;
+	}
+}
 
-    constructor(name) {
-      this.name = name;
-    }
-  
-  }
-  
-  class Rabbit extends Animal {
-    constructor(name) {
-      super (name);
-      this.created = Date.now();
-    }
-  }
-  
-  let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
-  alert(rabbit.name);
+class Rabbit extends Animal {
+	constructor(name) {
+		super(name);
+		this.created = Date.now();
+	}
+}
+
+let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+alert(rabbit.name);
